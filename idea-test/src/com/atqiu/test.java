a213f8f9f8c62083d7df1855c1d7b55bd58cbce5
2450788669 <2450788669@qq.com>
2020-05-24 20:38:56 +0800

    新增了一个类

---
+++ idea-test/src/com/atqiu/test.java
@@ -0,0 +1,11 @@
+package com.atqiu;
+
+/**
+ * @author 邱建伟
+ * @create 2020-05-24 20:36
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+    }
+}
